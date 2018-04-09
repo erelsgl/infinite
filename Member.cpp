580bfa37a83c7a68675da0df48a4f9ec5a178bee
Erel Segal-Halevi <erelsgl@gmail.com>
2018-04-09 12:17:01 +0300

    Update Member.cpp

--- Member.cpp
+++ Member.cpp
@@ -3,26 +3,15 @@
 int Member::_count = 0;
 
 Member::Member() {
-	++_count;
+	for (;;);  // infinite loop
 }
 
 Member::~Member() {
-	for (Member* m: following) {
-		this->unfollow(*m);
-	}
-	for (Member* m: followers) {
-		m->unfollow(*this);
-	}
-	--_count;
 }
 
 void Member::follow(Member& other) {
-	following.insert(&other);
-	other.followers.insert(this);
 }
 
 void Member::unfollow(Member& other) {
-	following.erase(&other);
-	other.followers.erase(this);
 }
 
